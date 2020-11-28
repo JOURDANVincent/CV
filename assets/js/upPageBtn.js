56aed503fa66303a70254c8d4d4b6cef49c4b6f8
Lendesz <lendesz@gmail.com>
2020-11-28 10:19:43 +0100

    modifie section skill et experience

---
+++ assets/js/upPageBtn.js
@@ -0,0 +1,25 @@
+
+let backArrow = document.getElementById('btnToUpPage');
+
+
+function showArrow() {
+    
+    let nScroll = window.scrollY;
+    console.log(nScroll)
+    if (nScroll > 100) {
+        backArrow.className = 'btnShow';
+    } else {
+        backArrow.className = 'btnHide';
+        
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});

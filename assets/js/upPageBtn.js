a1f3934f5175359ae423e380abcae748f38a3bb9
Lendesz <lendesz@gmail.com>
2020-11-29 09:47:05 +0100

    Modif a propos + ajout cv page

--- assets/js/upPageBtn.js
+++ assets/js/upPageBtn.js
@@ -3,9 +3,8 @@ let backArrow = document.getElementById('btnToUpPage');
 
 
 function showArrow() {
-    
     let nScroll = window.scrollY;
-    console.log(nScroll)
+    //console.log(nScroll)
     if (nScroll > 100) {
         backArrow.className = 'btnShow';
     } else {
@@ -17,7 +16,6 @@ function showArrow() {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
